f283f88f7ffa6cc254badba1418d2ec3975e32c7
Giulia Ferreira Gomes <78506785+giuliafgomes36@users.noreply.github.com>
2024-05-29 16:57:26 -0300

    Adição função de som para todas as teclas

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,12 +1,40 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    let tecla = listaDeTeclas[contador];
+    let instrumento = tecla.classList[1]; 
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function () {tocaSom()};
-    contador++;
+    //template string
+    tecla.onclick = function () { tocaSom(`#som_${instrumento}`) };
+
+    tecla.onkeydown = function (evento) { selecionaTecla(evento, tecla); }
+
+    tecla.onkeyup = function (evento) { desselecionaTecla(evento, tecla); }
+
+}
+
+function tocaSom (idElementoAudio) {
+   const elemento =  document.querySelector(idElementoAudio);
+
+   console.log(elemento.localName)
+   
+   if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+   } 
+   else {
+    alert('Elemento não encontrado');
+   }
+}
+
+function selecionaTecla (evento, tecla) {
+    if (evento.code === "Enter" || evento.code === "Space") {
+        tecla.classList.add('ativa');
+    }
+}
+
+function desselecionaTecla (evento, tecla) {
+    if (evento.code === "Enter" || evento.code === "Space") {
+        tecla.classList.remove('ativa');
+    }
 }
